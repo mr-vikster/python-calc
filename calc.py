724fd297bb1e7e1811f237008bcfa629fac47f3b
mr-vikster <mrvikster42@gmail.com>
2024-05-05 18:55:36 +0200

    minimal working colculator

--- calc.py
+++ calc.py
@@ -1,3 +1,40 @@
-def main (args):
-  print(args[0] + args[1])
+import sys
+def add(one, two):
+  return one + two
+
+def subtract(left_number, right_number):
+  return left_number - right_number
+
+def multiply(left_number, right_number):
+  return left_number * right_number
+
+def divide(left_number, right_number):
+  if(right_number == 0):
+    raise ZeroDivisionError("Ти що, дебіл? Ділити на нуль не можна!!") 
+  return round(left_number / right_number)
+
+def main():
+  left_number = int(sys.argv[1])
+  right_number = int(sys.argv[3])
+  operation = sys.argv[2]
+  result = None
+  print('left_number: ', left_number)
+  print('right_number: ', right_number)
+  print('operation: ', operation)
+
+  match operation:
+    case '+':
+      result = add(left_number, right_number)
+    case '-':
+      result = subtract(left_number, right_number)
+    case '*':
+      result = multiply(left_number, right_number)
+    case '/':
+      result = divide(left_number, right_number)
+
+    case _ :
+      raise ValueError('Unknown operation')
+
+  print('result is: ', result)
   
+main()
